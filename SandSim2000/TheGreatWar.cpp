93f61f6c0c148807d712e19f9682b2d808c7b7a8
Samuel K D L Davidson <123samueld@gmail.com>
2024-07-29 14:46:59 +0100

    Added Snow Pines

--- SandSim2000/TheGreatWar.cpp
+++ SandSim2000/TheGreatWar.cpp
@@ -23,7 +23,7 @@ int main()
 
     agentManager.placePathfinderAgent(sf::Vector2i(2, 2), &scene.gameScene, PathfinderAgent(2, 2, "PathfinderAgent"), gameStateManager);
 
-    int numberOfAgents = 10;
+    int numberOfAgents = 400;
     for(int i = 0; i < numberOfAgents; i++)
         agentManager.placeMobileAgent(sf::Vector2i(2, 2), &scene.gameScene, MobileAgent(2 , 2, 1, 1, 0.1f, 1, "RedBaron"), gameStateManager);
 
